ccdd9ff7f5dba2b5eebb2c3d708882266a3d77f9
Jhonatan <jhonatangoncalves2002@gmail.com>
2025-08-26 22:17:13 -0300

    :tada: Commit inicial - definição da estrutura para criação do projeto 01 do bootcamp

---
+++ project01-conta-banco/src/main/java/org/yourcompany/yourproject/Project01ContaBanco.java
@@ -0,0 +1,12 @@
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Jhonatan
+ */
+public class Project01ContaBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
