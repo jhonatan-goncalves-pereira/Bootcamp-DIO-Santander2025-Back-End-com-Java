32062b6cf4dd869ca53a76d81c12710db1c3f99e
Jhonatan <jhonatangoncalves2002@gmail.com>
2025-08-28 16:35:05 -0300

    :sparkles: feat: conclusão do desafio de projeto 01 - ContaBanco Terminal

--- project01-conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ project01-conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -1,5 +1,7 @@
 package org.yourcompany.yourproject;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jhonatan
@@ -7,6 +9,26 @@ package org.yourcompany.yourproject;
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = entrada.nextInt();
+        entrada.nextLine(); // limpar buffer do scanner
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = entrada.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = entrada.nextLine();
+
+        System.out.print("Por favor, digite o saldo inicial: ");
+        double saldo = entrada.nextDouble();
+
+        System.out.println("\nOlá " + nomeCliente + 
+                ", obrigado por criar uma conta em nosso banco, sua agência é " 
+                + agencia + ", conta " + numero + 
+                " e seu saldo " + saldo + " já está disponível para saque.");
+
+        entrada.close();
     }
 }
