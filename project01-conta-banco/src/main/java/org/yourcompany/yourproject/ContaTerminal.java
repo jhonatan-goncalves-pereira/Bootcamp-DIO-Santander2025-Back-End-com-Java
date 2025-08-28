48d1d95a9ccf6e874876652fa67b38a19654995b
Jhonatan <jhonatangoncalves2002@gmail.com>
2025-08-28 16:47:33 -0300

    :zap: perf: Melhoria na estrutura do código

--- project01-conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ project01-conta-banco/src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -1,10 +1,9 @@
-// -*- coding: UTF-8 -*-
 package org.yourcompany.yourproject;
 
 import java.util.Scanner;
 
 /**
- *
+ * Desafio ContaBanco - DIO / Santander 2025
  * @author Jhonatan
  */
 public class ContaTerminal {
@@ -12,23 +11,24 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.print("Por favor, digite o número da Conta: ");
-        int numero = entrada.nextInt();
+        System.out.print(">>> Por favor, digite o número da Conta: ");
+        final int numero = entrada.nextInt();
         entrada.nextLine(); // limpar buffer do scanner
 
-        System.out.print("Por favor, digite o número da Agência: ");
-        String agencia = entrada.nextLine();
+        System.out.print(">>> Por favor, digite o número da Agência: ");
+        final String agencia = entrada.nextLine();
 
-        System.out.print("Por favor, digite o nome do Cliente: ");
-        String nomeCliente = entrada.nextLine();
+        System.out.print(">>> Por favor, digite o nome do Cliente: ");
+        final String nomeCliente = entrada.nextLine();
 
-        System.out.print("Por favor, digite o saldo inicial: ");
-        double saldo = entrada.nextDouble();
+        System.out.print(">>> Por favor, digite o saldo inicial: ");
+        final double saldo = entrada.nextDouble();
 
-        System.out.println("\nOlá " + nomeCliente + 
-                ", obrigado por criar uma conta em nosso banco, sua agência é " 
-                + agencia + ", conta " + numero + 
-                " e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.printf(
+            "%nOlá %s, obrigado por criar uma conta em nosso banco!%n" +
+            "Sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque.%n",
+            nomeCliente, agencia, numero, saldo
+        );
 
         entrada.close();
     }
